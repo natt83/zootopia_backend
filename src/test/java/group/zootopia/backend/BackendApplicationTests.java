6b9232f0d902613bbe4ce1cf2869fe5a8cddcefa
CarlosUribe225 <carurbe225@gmail.com>
2024-01-15 14:21:01 +0100

    first commit

---
+++ src/test/java/group/zootopia/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package group.zootopia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
